8ddc6ddabbf7078d12fc6720991d864c94d45219
himanshuweb3 <99200713+himanshuweb3@users.noreply.github.com>
2022-02-08 19:24:57 +0530

    Update About.js

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -70,7 +70,7 @@ const About = () => {
 <div className='col-xl-6 col-lg-7'>
               <div className='about-three__content-box'>
                 <div className='sec-title'>
-                <h2 className='sec-title__title'>
+                <h2 className='sec-title__title'><br/>
                     Our Mission
                   </h2>
                 </div>
@@ -93,6 +93,19 @@ const About = () => {
       </section>
       {/*About Three End*/}
      
+<div className='container our-story '>
+          <div className='text-center clearfix'>
+            
+            <h2>Our Story</h2>
+          </div>
+<img src="https://escrowindia.globalgroominghealthcare.com/assets/images/about/03.png" />
+  <p>Krishi Square is B2B agri tech platform to enable discovery and trust in trade for small and medium size businesses(SMBs) and Corporates.
+<br />
+We believe the world will look very different after this pandemic, as it did after the second world war and the financial crisis of 2008. We foresee a gigantic acceleration towards digitalization in the B2B space. B2B digitization has lagged behind its B2C peers due to an inability to solve for trust deficit which currently exists in SMB and Corporate market. Technology will acts as an enabler and will play a vital role to achieve a humongous scale in $300B agri industry.
+
+</p>
+        </div>
+
       
     </>
   );
