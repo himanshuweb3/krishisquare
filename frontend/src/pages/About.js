25f3b62ea9a18f67d4b2c3fa2b2f41c6918521d6
himanshuweb3 <99200713+himanshuweb3@users.noreply.github.com>
2022-02-08 19:16:07 +0530

    Update About.js

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -20,7 +20,7 @@ const About = () => {
       {/* Page Header End*/}
       {/*About Three Start*/}
       <section className='about-three'>
-        <div className='about-three__shape'></div>
+      
         {/* /.about-three__shape */}
         <div className='container'>
           <div className='row'>
@@ -28,22 +28,15 @@ const About = () => {
             <div className='col-xl-6 col-lg-7'>
               <div className='about-three__content-box'>
                 <div className='sec-title'>
-                 
-                  
-                  <h2 className='sec-title__title'>
+                <h2 className='sec-title__title'>
                     Our Vision
                   </h2>
                 </div>
                 <div className='about-three__content-box-inner'>
-                 
                   <p>
                     Building trust in trade with digital technologies to bring transparency in agri business.
                   </p>
-
-                 
                 </div>
-               
-                
                 {/* /.about-three__arrow */}
               </div>
             </div>
@@ -52,11 +45,6 @@ const About = () => {
             {/*Start About Three Img Box*/}
             <div className='col-xl-6 col-lg-5'>
               <div className='about-three__img-box'>
-                <img
-                  src='assets/images/resources/about-3-icon-1-1.png'
-                  className='about-three__img-icon'
-                  alt=''
-                />
                 <div className='about-three__img-box-img'>
                   <div className='about-three__img-box-img-inner'>
                     <img src='https://escrowindia.globalgroominghealthcare.com/assets/aboutus.png' alt='' />
@@ -65,6 +53,41 @@ const About = () => {
               </div>
             </div>
             {/*End About Three Img Box*/}
+
+
+
+ {/*Start About Three Img Box*/}
+            <div className='col-xl-6 col-lg-5'>
+              <div className='about-three__img-box'>
+                <div className='about-three__img-box-img'>
+                  <div className='about-three__img-box-img-inner'>
+                    <img src='https://escrowindia.globalgroominghealthcare.com/assets/images/about/02.jpg' alt='' />
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/*End About Three Img Box*/}
+<div className='col-xl-6 col-lg-7'>
+              <div className='about-three__content-box'>
+                <div className='sec-title'>
+                <h2 className='sec-title__title'>
+                    Our Mission
+                  </h2>
+                </div>
+                <div className='about-three__content-box-inner'>
+                  <p>
+                    Empower SMB's and corporates to trade through a safe and a simple platform.
+                  </p>
+                </div>
+                {/* /.about-three__arrow */}
+              </div>
+            </div>
+            {/*End About Three Content Box*/}
+
+           
+
+
+
           </div>
         </div>
       </section>
