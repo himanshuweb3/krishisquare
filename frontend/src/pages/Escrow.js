81bf5f96fd1fdcdaf90685c5e8d1c89a26a2113b
himanshuweb3 <99200713+himanshuweb3@users.noreply.github.com>
2022-02-09 13:36:48 +0530

    Update Escrow.js

--- frontend/src/pages/Escrow.js
+++ frontend/src/pages/Escrow.js
@@ -1,9 +1,86 @@
 import React from 'react';
 
 const Escrow = () => {
-  return <div>
-    <h1 className="heading-primary text-center">Escrow Page</h1>
-    </div>;
+  return (
+    <>
+      {/* {/*Page Header Start*/}
+      <section
+        className='page-header clearfix'
+        style={{
+          backgroundImage: "url(https://escrowindia.globalgroominghealthcare.com/assets/images/about/03.png)",
+        }}
+      >
+        <div className='container'>
+          <div className='page-header__inner text-center clearfix'>
+            
+            <h2>Escrow</h2>
+          </div>
+        </div>
+      </section>
+      {/* Page Header End*/}
+
+<div className='container aboutintro'>
+          <div className='row'>
+            <div className='col-xl-12 col-lg-12'>
+              <h3>How Escrow Works</h3>
+            </div>
+</div>
+</div>
+
+      {/*About Three Start*/}
+      <section className='about-three'>
+      
+        {/* /.about-three__shape */}
+        <div className='container'>
+          <div className='row'>
+            {/*Start About Three Content Box*/}
+            <div className='col-xl-12 col-lg-12'>
+              <div className='about-three__content-box'>
+              
+                <h3>Protect your payments with a fully digital escrow </h3>
+<p>Buyer's money is securely held and released to the seller post quality check:</p>
+
+<p><img src="https://escrowindia.globalgroominghealthcare.com/assets/escrow1.png" /> Got a great deal from a new buyer, but don't trust him with payments ?</p>
+
+ <p><img src="https://escrowindia.globalgroominghealthcare.com/assets/escrow2.png" />Found a good supplier at right price, but don't trust with the quality ?</p>
+
+<h4><img src="https://escrowindia.globalgroominghealthcare.com/assets/buyer.jpg" />Buyer</h4>
+<table>
+<tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" /> Protect your transactions against delays and cancellations</td></tr>
+<tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" /> No more unsecured advance payments</td></tr>
+<tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" />Keep your money secure until goods are QC at receipt</td></tr>
+<tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" /> Access new sellers and markets to grow your business</td></tr>
+<tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" />Fully verify seller's identity</td></tr>
+<tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" />Get your money back if the goods fail the inspection</td></tr>
+</table>
+              </div>
+            </div>
+            {/*End About Three Content Box*/}
+
+
+          </div>
+        </div>
+      </section>
+      {/*About Three End*/}
+     
+<div className='container our-story '>
+          <div className='text-center clearfix'>
+            
+            <h2>Our Story</h2>
+          </div>
+<img src="https://escrowindia.globalgroominghealthcare.com/assets/images/about/03.png" />
+  <p>Krishi Square is B2B agri tech platform to enable discovery and trust in trade for small and medium size businesses(SMBs) and Corporates.
+<br />
+We believe the world will look very different after this pandemic, as it did after the second world war and the financial crisis of 2008. We foresee a gigantic acceleration towards digitalization in the B2B space. B2B digitization has lagged behind its B2C peers due to an inability to solve for trust deficit which currently exists in SMB and Corporate market. Technology will acts as an enabler and will play a vital role to achieve a humongous scale in $300B agri industry.
+
+</p>
+        </div>
+
+
+
+      
+    </>
+  );
 };
 
 export default Escrow;
