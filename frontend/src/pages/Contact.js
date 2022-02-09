f6d0d1258a960646b8a756b6398f0d8f3468e59b
himanshuweb3 <99200713+himanshuweb3@users.noreply.github.com>
2022-02-09 11:27:17 +0530

    Update Contact.js

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -63,7 +63,7 @@ const Contact = () => {
             {/*Start Contact Page Left */}
             <div className='col-xl-6 col-lg-6'>
               <div className='contact-page__left'>
-              
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3771.8003113255645!2d72.84410961490045!3d19.02851898711511!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7ced6c5e64c0f%3A0xf9d8cb93831fe9dd!2s144%2C%20Senapati%20Bapat%20Marg%2C%20Mahalaxmi%20Sindhi%20Colony%2C%20Matunga%20West%2C%20Mumbai%2C%20Maharashtra%20400016!5e0!3m2!1sen!2sin!4v1644386152157!5m2!1sen!2sin" width="600" height="450" style={{border:"0"}} allowfullscreen="" loading="lazy"></iframe>
             </div>
              </div>
             {/*End Contact Page Right */}
