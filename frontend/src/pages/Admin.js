7193eed8756b215462f8831b62ba3db27727dda6
himanshuweb3 <99200713+himanshuweb3@users.noreply.github.com>
2022-02-09 19:39:23 +0530

    Update Admin.js

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -33,7 +33,7 @@ const Admin = ({ user, setUser }) => {
       style={{ minHeight: "80vh" }}
       className='d-flex flex-wrap'
     >
-      <div  style={{ width: "280px", background-color: '#f5f6fa', border-right-color: '#e5e9f2' }} className='flex-shrink-0 p-3'>
+      <div style={{ width: '280px', background-color: '#f5f6fa', border-right-color: '#e5e9f2' }} className='flex-shrink-0 p-3' >
         <a
           href='/'
           className='d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom'
@@ -41,7 +41,7 @@ const Admin = ({ user, setUser }) => {
           {/* <svg className='bi me-2' width='30' height='24'>
             <use xlink:href='#bootstrap' />
           </svg> */}
-          <span className='fw-semibold' style={{ fonts-size: '30px' }}>Welcome Admin</span>
+          <span style={{ fonts-size: '30px' }} className='fw-semibold' > Welcome Admin </span>
         </a>
         <ul className='list-unstyled ps-0'>
           <li className='mb-1'>
