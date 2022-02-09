3ea371f3ed021697c43530f6275baee5b89f5775
himanshuweb3 <99200713+himanshuweb3@users.noreply.github.com>
2022-02-09 18:30:44 +0530

    Update Insights.js

--- frontend/src/pages/Insights.js
+++ frontend/src/pages/Insights.js
@@ -1,13 +1,32 @@
 import React from "react";
 
 const Insights = () => {
-  return (
+   return (
     <>
-      <div className='m-5 d-flex justify-content-center'>
-        <img src='/assets/images/backgrounds/insights.jpg' alt='' />
-      </div>
-      <h1 className='heading-primary text-center mb-5'>Coming Soon..</h1>
-    </>
+      {/* {/*Page Header Start*/}
+      <section
+        className='page-header clearfix'
+        style={{
+          backgroundImage: "url(https://escrowindia.globalgroominghealthcare.com/assets/images/about/03.png)",
+        }}
+      >
+        <div className='container'>
+          <div className='page-header__inner text-center clearfix'>
+            
+            <h2>Insights</h2>
+          </div>
+        </div>
+      </section>
+      {/* Page Header End*/}
+
+<div className='container aboutintro'>
+          <div className='row'>
+            <div className='col-xl-12 col-lg-12 text-center'>
+              <h2>Coming Soon</h2>
+            </div>
+</div>
+</div>
+
   );
 };
 
