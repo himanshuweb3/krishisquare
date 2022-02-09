deb5dfbc53bc4a621595a1e04362f270a3530377
himanshuweb3 <99200713+himanshuweb3@users.noreply.github.com>
2022-02-09 18:37:22 +0530

    Update Insights.js

--- frontend/src/pages/Insights.js
+++ frontend/src/pages/Insights.js
@@ -9,23 +9,23 @@ const Insights = () => {
         style={{
           backgroundImage: "url(https://escrowindia.globalgroominghealthcare.com/assets/images/about/03.png)",
         }}
-      >
+        >
+   
         <div className='container'>
           <div className='page-header__inner text-center clearfix'>
-            
             <h2>Insights</h2>
           </div>
         </div>
       </section>
       {/* Page Header End*/}
 
-<div className='container aboutintro'>
+       <div className='container'>
           <div className='row'>
             <div className='col-xl-12 col-lg-12 text-center'>
               <h2>Coming Soon</h2>
-            </div>
-</div>
-</div>
+           </div>
+         </div>
+       </div>
 
   );
 };
