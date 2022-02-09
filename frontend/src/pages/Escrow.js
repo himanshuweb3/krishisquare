463835bdb68991a73a0974cebf2d97744aed6d47
himanshuweb3 <99200713+himanshuweb3@users.noreply.github.com>
2022-02-09 18:12:40 +0530

    Update Escrow.js

--- frontend/src/pages/Escrow.js
+++ frontend/src/pages/Escrow.js
@@ -42,26 +42,26 @@ const Escrow = () => {
 
 <p><img src="https://escrowindia.globalgroominghealthcare.com/assets/escrow1.png" /> Got a great deal from a new buyer, but don't trust him with payments ?</p>
 
- <p><img src="https://escrowindia.globalgroominghealthcare.com/assets/escrow2.png" />Found a good supplier at right price, but don't trust with the quality ?</p>
+ <p><img src="https://escrowindia.globalgroominghealthcare.com/assets/escrow2.png" /> Found a good supplier at right price, but don't trust with the quality ?</p>
 
 <h4><img src="https://escrowindia.globalgroominghealthcare.com/assets/buyer.jpg" />Buyer</h4>
 <table>
 <tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" /> Protect your transactions against delays and cancellations</td></tr>
 <tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" /> No more unsecured advance payments</td></tr>
-<tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" />Keep your money secure until goods are QC at receipt</td></tr>
+<tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" /> Keep your money secure until goods are QC at receipt</td></tr>
 <tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" /> Access new sellers and markets to grow your business</td></tr>
-<tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" />Fully verify seller's identity</td></tr>
-<tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" />Get your money back if the goods fail the inspection</td></tr>
+<tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" /> Fully verify seller's identity</td></tr>
+<tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" /> Get your money back if the goods fail the inspection</td></tr>
 </table>
 
 <h4><img src="https://escrowindia.globalgroominghealthcare.com/assets/seller.jpg" />Seller</h4>
 <table>
 <tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" /> Get paid on time and in full orders</td></tr>
 <tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" /> No need to ship goods until payment is ensued</td></tr>
-<tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" />Sell more by building trust with buyers</td></tr>
+<tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" /> Sell more by building trust with buyers</td></tr>
 <tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" /> Secure full payment amount in an escrow before you ship</td></tr>
-<tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" />Build your reputation as a reliable seller</td></tr>
-<tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" />Work with verified buyers, eliminating post shipment hassles</td></tr>
+<tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" /> Build your reputation as a reliable seller</td></tr>
+<tr><td><img src="https://escrowindia.globalgroominghealthcare.com/assets/images/bg-images/chk.png" /> Work with verified buyers, eliminating post shipment hassles</td></tr>
 </table>
 
               </div>
@@ -87,7 +87,7 @@ const Escrow = () => {
       <div className="lab-inner">
         <div className="lab-thumb"><img src="assets\images\services\01.png" alt="lab-thumb" /></div>
 <div className="lab-content"><button className="lab-btn btn-sm text-white">Step 1</button><br /><br /><a href="/escrow">
-<h5>Send Agreement</h5></a><br /><p>Provide transaction details and send an agreement with terms and conditions to buyer and seller</p>
+<h5>Send Agreement</h5></a><p>Provide transaction details and send an agreement with terms and conditions to buyer and seller</p>
 </div></div></div></div>
   
   <div className="col-xl-4 col-md-6 col-12">
@@ -95,33 +95,33 @@ const Escrow = () => {
       <div className="lab-inner">
         <div className="lab-thumb"><img src="assets\images\services\02.png" alt="lab-thumb" /></div>
 <div className="lab-content"><button className="lab-btn btn-sm text-white">Step 2</button><br /><br /><a href="/escrow">
-<h5>Accept Terms and Conditions</h5></a><br /><p>Buyer and seller to agree on terms and conditions on product quantity, price, quality, shipping information and payment release condition</p>
+<h5>Accept Terms and Conditions</h5></a><p>Buyer and seller to agree on terms and conditions on product quantity, price, quality, shipping information and payment release condition</p>
 </div></div></div></div>
   
   <div className="col-xl-4 col-md-6 col-12"><div className="lab-item"><div className="lab-inner">
     <div className="lab-thumb"><img src="assets\images\services\03.png" alt="lab-thumb" /></div>
 <div className="lab-content"><button className="lab-btn btn-sm text-white">Step 3</button><br /><br /><a href="/escrow">
-      <h5>Transfer Payment to Escrow</h5></a><br /><p>Buyer to transfer the invoice amount to escrow account, where funds will be parked until the order is fulfilled</p>
+      <h5>Transfer Payment to Escrow</h5></a><p>Buyer to transfer the invoice amount to escrow account, where funds will be parked until the order is fulfilled</p>
 </div></div></div></div>
 
   <div className="col-xl-4 col-md-6 col-12"><div className="lab-item"><div className="lab-inner">
     <div className="lab-thumb"><img src="assets\images\services\04.png" alt="lab-thumb" /></div>
-<div className="lab-content"><button className="lab-btn btn-sm text-white">Step 4</button><br /><br /><a href="/escrow"><h5>Provide Document Proof</h5></a><br /><p>Seller to provide proof of shipment for goods Inspection period</p>
+<div className="lab-content"><button className="lab-btn btn-sm text-white">Step 4</button><br /><br /><a href="/escrow"><h5>Provide Document Proof</h5></a><p>Seller to provide proof of shipment for goods Inspection period</p>
 </div></div></div></div>
   
   <div className="col-xl-4 col-md-6 col-12"><div className="lab-item"><div className="lab-inner">
     <div className="lab-thumb"><img src="assets\images\services\05.png" alt="lab-thumb" /></div>
-<div className="lab-content"><button className="lab-btn btn-sm text-white">Step 5</button><br /><br /><a href="/escrow"><h5>Inspection Period</h5></a><br /><p>On receipt of goods, buyer gets a inspection period during which they give a go ahead if the quality is fine else sent back to seller</p>
+<div className="lab-content"><button className="lab-btn btn-sm text-white">Step 5</button><br /><br /><a href="/escrow"><h5>Inspection Period</h5></a><p>On receipt of goods, buyer gets a inspection period during which they give a go ahead if the quality is fine else sent back to seller</p>
 </div></div></div></div>
   
   <div className="col-xl-4 col-md-6 col-12"><div className="lab-item"><div className="lab-inner">
     <div className="lab-thumb"><img src="assets\images\services\06.png" alt="lab-thumb" /></div>
-<div className="lab-content"><button className="lab-btn btn-sm text-white">Step 6</button><br /><br /><a href="/escrow"><h5>Document verification</h5></a><br /><p>Documents to be checked against agreement for payment release if the quality is fine by buyer</p>
+<div className="lab-content"><button className="lab-btn btn-sm text-white">Step 6</button><br /><br /><a href="/escrow"><h5>Document verification</h5></a><p>Documents to be checked against agreement for payment release if the quality is fine by buyer</p>
 </div></div></div></div>
   
   <div className="col-xl-4 col-md-6 col-12"><div className="lab-item"><div className="lab-inner">
     <div className="lab-thumb"><img src="assets\images\services\01.png" alt="lab-thumb" /></div>
-<div className="lab-content"><button className="lab-btn btn-sm text-white">Step 7</button><br /><br /><a href="/escrow"><h5>Payment Release</h5></a><br /><p>Once documents are verified, payment will be released and seller will receive funds within 48 hours</p>
+<div className="lab-content"><button className="lab-btn btn-sm text-white">Step 7</button><br /><br /><a href="/escrow"><h5>Payment Release</h5></a><p>Once documents are verified, payment will be released and seller will receive funds within 48 hours</p>
 </div></div></div></div>
   
  </div>
