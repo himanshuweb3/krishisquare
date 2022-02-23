37082814ee2f2de49673c53c59d8806eda4aabc7
himanshuweb3 <99200713+himanshuweb3@users.noreply.github.com>
2022-02-23 16:00:25 +0530

    Add files via upload

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,10 @@ const router = express.Router();
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
+
+router.route("/password/forgot").post(authController.forgotPassword);
+router.route("/password/reset/:token").put(authController.resetPassword);
+
 router.delete("/:id", userController.deleteUser);
 router.get("/me", authController.protect, userController.getUserDetails);
 router.get("/logout", authController.logout);
