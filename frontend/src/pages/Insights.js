9a613116a2e295dcf3fa6db19fd809ad6b0a0fa9
himanshuweb3 <99200713+himanshuweb3@users.noreply.github.com>
2022-02-28 18:44:18 +0530

    Update Insights.js

--- frontend/src/pages/Insights.js
+++ frontend/src/pages/Insights.js
@@ -21,8 +21,29 @@ const Insights = () => {
 
        <div className='container' style={{ padding:'100px 0px' }}>
           <div className='row'>
-            <div className='col-xl-12 col-lg-12 text-center'>
-              <h2>Coming Soon</h2>
+            <div className='col-xl-3 col-lg-6 '>
+               <img src="assets/images/insight1.jpg" />
+              <h3>Future of Indian Agriculture - Vision 2030</h3>
+              <p>The insights we reached are consequential and sometimes counterintuitive. <p>
+               <a href="https://www.omnivore.vc/vision-2030/" target="_blank">read more</a>
+           </div>
+            <div className='col-xl-3 col-lg-6 '>
+               <img src="assets/images/insight2.jpg" />
+              <h3>Indian Agricultural transformation</h3>
+              <p>Indian Agricultural transformation. <p>
+               <a href="https://www.google.com/url?sa=t&source=web&rct=j&url=https://assets.ey.com/content/dam/ey-sites/ey-com/en_in/topics/start-ups/2020/09/ey-agritech-towards-transforming-indian-agriculture.pdf%3Fdownload&ved=2ahUKEwiI-rKZ6pz2AhX3QPUHHZFYBaYQFnoECBYQAQ&usg=AOvVaw0v_PA9iut890kdGeCkiArD" target="_blank">read more</a>
+           </div>
+            <div className='col-xl-3 col-lg-6 '>
+               <img src="assets/images/insight3.jpg" />
+              <h3>Agritech Innovations</h3>
+              <p>Agritech Innovations <p>
+               <a href="https://www.google.com/url?sa=t&source=web&rct=j&url=https://fedev.org/wp-content/uploads/2020/03/AI-TIA%2520Report%2520V7.pdf&ved=2ahUKEwiD2MWr7Jz2AhVWSmwGHYmeA50QFnoECBMQAQ&usg=AOvVaw3TLAnz9Q6-3p8S4uCBeMHM" target="_blank">read more</a>
+           </div>
+           <div className='col-xl-3 col-lg-6 '>
+               <img src="assets/images/insight4.jpg" />
+              <h3>NABARD Sustainability report</h3>
+              <p>NABARD Sustainability report <p>
+               <a href="https://www.google.com/url?sa=t&source=web&rct=j&url=https://www.nabard.org/auth/writereaddata/tender/2909214958sustainability-report-2019-20.pdf&ved=2ahUKEwj95bnU7Jz2AhWQSmwGHVG8A7kQFnoECAQQAQ&usg=AOvVaw2M1Q0FIjvh152kr26Aqa2-" target="_blank">read more</a>
            </div>
          </div>
        </div>
